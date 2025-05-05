04ad0757664da09e4e33d6e897904c1b93de4c6c
NachoL24 <ignaciolances24@gmail.com>
2025-05-04 22:42:41 -0300

    cambio de layout

--- juego-adivinar-numero-aleatorio/app/build.gradle.kts
+++ juego-adivinar-numero-aleatorio/app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
